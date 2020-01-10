7db6b1f5d200947026d2fb35ec4e78bb0a303566
angela <angelahidort@gmail.com>
2020-01-10 10:34:08 +0100

    vista create de instrumentos

---
+++ vistas_instrumentos/create.php
@@ -0,0 +1,208 @@
+<?php
+/*//error_reporting(E_ALL & ~(E_STRICT|E_NOTICE));
+session_start();
+if (!isset($_SESSION['USUARIO']['email'])) {
+    //echo $_SESSION['USUARIO']['email'];
+    //exit();
+    header("location: login.php");
+    exit();
+}*/
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/musihub/dirs.php";
+require_once CONTROLLER_PATH . "ControladorInstrumento.php";
+require_once CONTROLLER_PATH . "ControladorImagen.php";
+require_once UTILITY_PATH . "funciones.php";
+
+// Variables
+$nombre = $referencia = $compañiadistribuidor = $tipo =  $precio = $descuento =  $stockinicial =  $imagen = "";
+$nombreVal = $referenciaVal = $compañiadistribuidorVal = $tipoVal =  $precioVal = $descuentoVal =  $stockinicialVal =  $imagenVal = "";
+$nombreErr = $referenciaErr = $compañiadistribuidorErr = $tipoErr =  $precioErr = $descuentoErr =  $stockinicialErr =  $imagenErr = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["aceptar"]) {
+    // Procesamos el nombre
+    $nombreVal = filtrado(($_POST["nombre"]));
+    if (empty($nombreVal)) {
+        $nombreErr = "Por favor introduzca un nombre válido con solo carácteres alfabéticos.";
+    } elseif (!preg_match("/([^\s][A-zÀ-ž\s]+$)/", $nombreVal)) {
+        $nombreErr = "Por favor introduzca un nombre válido con solo carácteres alfabéticos.";
+    } else {
+        $nombre = $nombreVal;
+    }
+    // NO SE REPITA el nombre (si se quiere otro campo modificar el ControladorInstrumento en la funcion buscarInstrumento)
+    $controlador = ControladorInstrumento::getControlador();
+    $instrumento = $controlador->buscarInstrumentoNom($nombre);
+    if (isset($instrumento)) {
+        $nombreErr = "Ya existe un instrumento con este nombre:" . $nombreVal . " en la Base de Datos";
+    } else {
+        $nombre = $nombreVal;
+    }
+
+    // Procesamos referencia
+    if (isset($_POST["referencia"])) {
+        $referencia = filtrado($_POST["referencia"]);
+    } elseif (!preg_match("/([1-9])/", $referenciaVal)) {
+        $referenciaErr = "Introduzca una referencia valida en numeros";
+    } else {
+        $referencia = $referenciaVal;
+    }
+
+    // Procesamos compañiadistribuidor
+    if (isset($_POST["compañiadistribuidor"])) {
+        $compañiadistribuidor = filtrado($_POST["compañiadistribuidor"]);
+    } else {
+        $compañiadistribuidorErr = "Debe elegir al menos una compañiadistribuidor";
+    }
+
+    // Procesamos tipo
+    if (isset($_POST["tipo"])) {
+        $tipo = filtrado($_POST["tipo"]);
+    } else {
+        $tipoErr = "Tipo incorrecto";
+    }
+
+    // Procesamos precio
+    if (isset($_POST["precio"])) {
+        $precio = filtrado($_POST["precio"]);
+    } else {
+        $precioErr = "Precio incorrecto";
+    }
+
+    // Procesamos descuento
+    if (isset($_POST["descuento"])) {
+        $descuento = filtrado($_POST["descuento"]);
+    } else {
+        $descuentoErr = "descuento incorrecto";
+    }
+
+    // Procesamos stockinicial
+    if (isset($_POST["stockinicial"])) {
+        $stockinicial = filtrado($_POST["stockinicial"]);
+    } elseif (!preg_match("/([0-9])/", $stockinicialVal)) {
+        $stockinicialErr = "Introduzca un stockinicial valido desde el 0";
+    } else {
+        $stockinicial = $stockinicialVal;
+    }
+
+    // Procesamos la foto
+    $propiedades = explode("/", $_FILES['imagen']['type']);
+    $extension = $propiedades[1];
+    $tam_max = 50000; // 50 KBytes
+    $tam = $_FILES['imagen']['size'];
+    $mod = true; // para modificar
+
+    // Si no coicide la extensión
+    if ($extension != "jpg" && $extension != "jpeg") {
+        $mod = false;
+        $imagenErr = "Formato debe ser jpg/jpeg";
+    }
+    // si no tiene el tamaño
+    if ($tam > $tam_max) {
+        $mod = false;
+        $imagenErr = "Tamaño superior al limite de: " . ($tam_max / 1000) . " KBytes";
+    }
+
+    if ($mod) {
+        //guardar imagen
+        $imagen = md5($_FILES['imagen']['tmp_name'] . $_FILES['imagen']['name'] . time()) . "." . $extension;
+        $controlador = ControladorImagen::getControlador();
+        if (!$controlador->salvarImagen($imagen)) {
+            $imagenErr = "Error al procesar la imagen y subirla al servidor";
+        }
+    }
+    /*
+    //COMPROBAR SOLO ESTOS FILTROS PARA VER SI PASAN LOS DATOS
+
+    echo $nombreVal = filtrado(($_POST["nombre"])).  "<br>";
+    echo $raza = filtrado(implode(", ", $_POST["raza"])) .  "<br>";
+    echo $ki = filtrado($_POST["ki"]) .  "<br>";
+    echo $transformacion = filtrado($_POST["transformacion"]) .  "<br>";
+    echo $ataque = filtrado($_POST["ataque"]).  "<br>"; 
+    echo $planeta = filtrado($_POST["planeta"]) .  "<br>";
+    echo $password = filtrado($_POST["password"]) .  "<br>";
+    echo $fecha = filtrado($_POST["fecha"]) .  "<br>";
+    echo $imagen = filtrado(($_POST["imagen"]));
+*/
+    if (
+        empty($nombreErr) && empty($referenciaErr) && empty($compañiadistribuidorErr) && empty($tipoErr) &&
+        empty($precioErr) && empty($descuentoErr) && empty($stockinicialErr) && empty($imagenErr)
+    ) {
+        // creamos el controlador de dragones
+        $controlador = ControladorInstrumento::getControlador();
+        $estado = $controlador->almacenarInstrumento($nombre, $referencia, $compañiadistribuidor, $tipo, $precio, $descuento, $stockinicial, $imagen);
+        if ($estado) {
+            header("location: ../index.php");
+            exit();
+        } else {
+            header("location: error.php");
+            exit();
+        }
+    } else {
+        alerta("Hay errores al procesar el formulario revise los errores");
+    }
+} 
+
+?>
+
+<?php require_once VIEW_PATH . "cabecera.php"; ?>
+
+<h2>Crear Dragon</h2>
+
+<p>Por favor rellene este formulario para añadir un nuevo dragon a la base de datos de la clase.</p>
+<!-- Formulario-->
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+    <!-- Nombre-->
+    <div <?php echo (!empty($nombreErr)) ? 'error: ' : ''; ?>>
+        <label>Nombre</label>
+        <input type="text" required name="nombre" pattern="([^\s][A-zÀ-ž\s]+)" title="El nombre no puede contener números" value="<?php echo $nombre; ?>">
+       <?php echo $nombreErr; ?>
+    </div>
+    <!-- referencia -->
+    <div <?php echo (!empty($referenciaErr)) ? 'error: ' : ''; ?>>
+        <label>Referencia</label>
+        <input type="number" required name="referencia" pattern="([1-9])" title="Referencia valida solo con numeros" value="<?php echo $referencia; ?>">
+       <?php echo $referenciaErr; ?>
+    </div>
+    <!-- compañiadistribuidor -->
+    <div <?php echo (!empty($compañiadistribuidorErr)) ? 'error: ' : ''; ?>>
+        <label>Compañia / Distribuidor</label>
+        <input type="text" required name="compañiadistribuidor" pattern="([^\s][A-zÀ-ž\s]+)" title="Inserte el nombre de la compañia o distribuidor" value="<?php echo $compañiadistribuidor; ?>">
+        <?php echo $compañiadistribuidorErr; ?>
+    </div>
+    <!-- tipo-->
+        <label>Tipo de instrumento</label>
+        <select name="tipo">
+            <option value="percusion" <?php echo (strstr($tipo, 'percusion')) ? : ''; ?>>Percusion</option>
+            <option value="viento" <?php echo (strstr($tipo, 'viento')) ? : ''; ?>>Viento</option>
+            <option value="cuerda" <?php echo (strstr($tipo, 'cuerda')) ? : ''; ?>>Cuerda</option>
+            <option value="vientometal" <?php echo (strstr($tipo, 'vientometal')) ? 'selected' : ''; ?>>Viento Metal</option>
+        </select>
+    <!-- Precio -->
+    <div <?php echo (!empty($precioErr)) ? 'error: ' : ''; ?>>
+        <label>Precio</label>
+        <input type="number" required name="precio" value="<?php echo $precio; ?>">
+       <?php echo $precioErr; ?>
+    </div>
+    <!-- descuento-->
+    <div <?php echo (!empty($descuentoErr)) ? 'error: ' : ''; ?>>
+        <label>Descuento</label>
+        <input type="number" name="descuento" minlength="1">
+        <?php echo $descuentoErr; ?>
+    </div>
+    <!-- stockinicial -->
+    <div <?php echo (!empty($stockinicialErr)) ? 'error: ' : ''; ?>>
+        <label>Stock Inicial</label>
+        <input type="number" required name="stockinicial" pattern="([1-9])" title="Stock valido solo con numeros" value="<?php echo $stockinicial; ?>">
+       <?php echo $stockinicialErr; ?>
+    </div>
+    <!-- Foto-->
+    <div <?php echo (!empty($imagenErr)) ? 'error: ' : ''; ?>>
+        <label>Fotografía</label>
+        <input type="file" required name="imagen" id="imagen" accept="image/jpeg">
+        <?php echo $imagenErr; ?>
+    </div>
+    <!-- Botones -->
+    <button type="submit" name="aceptar" value="aceptar" > Aceptar</button>
+    <button type="reset" value="reset" > Limpiar</button>
+    <a href="../index.php" > Volver</a>
+</form>
+<br><br><br>

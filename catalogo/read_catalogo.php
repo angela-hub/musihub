fac2a3bc91ad1f1c1f0009bdcfd1eb50e9ed0d0e
angela <angelahidort@gmail.com>
2020-01-25 14:24:37 +0100

    Estilo a los detalles de los instrumentos

--- catalogo/read_catalogo.php
+++ catalogo/read_catalogo.php
@@ -1,3 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registro</title>
+    <link rel="icon" type="image/png" href="logo.png">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <style type="text/css">
+        .colorgraph {
+            height: 5px;
+            border-top: 0;
+            background: #c4e17f;
+            border-radius: 5px;
+            background-image: -webkit-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+            background-image: -moz-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+            background-image: -o-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+            background-image: linear-gradient(to right, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+        }
+    </style>
+</head>
+
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/musihub/dirs.php";
 require_once CONTROLLER_PATH . "ControladorInstrumento.php";
@@ -14,40 +40,59 @@ if (isset($_GET["id"]) && !empty(trim($_GET["id"]))) {
 }
 ?>
 
-<?php //require_once VIEW_PATH . "cabecera.php"; ?>
+<?php //require_once VIEW_PATH . "cabecera.php"; 
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+            <h1><center>Detalles del Instrumento</center></h1>
+            <hr class="colorgraph">
+            <table style=>
+                <tr>
+                    <td>
+                        <big><label>Nombre</label></big>
+                        <p><?php echo $instrumento->getnombre(); ?></p>
+                    </td>
+                    <td style="position:absolute; left:400px;">
+                        <big><label>Fotografía</label><br></big>
+                        <img src='<?php echo "../imagenes/fotos/" . $instrumento->getimagen() ?>' class='rounded' class='img-thumbnail' width='150' height='auto'>
+                    </td>
+                </tr>
+            </table>
+            <br>
+            <center>
+                <label>Referencia</label>
+                <p><?php echo $instrumento->getreferencia(); ?></p>
+            </center>
+            <br>
+            <center>
+                <label>Distribuidor</label>
 
-<h1>Detalles del Instrumento</h1>
-<table style=>
-    <tr>
-        <td>
-            <label>Nombre</label>
-            <p><?php echo $instrumento->getnombre(); ?></p>
-        </td>
-        <td style="position:absolute; left:200px;">
-            <label>Fotografía</label><br>
-            <img src='<?php echo "../imagenes/fotos/" . $instrumento->getimagen() ?>' class='rounded' class='img-thumbnail' width='48' height='auto'>
-        </td>
-    </tr>
-</table>
-<br>
-<label>Referencia</label>
-<p><?php echo $instrumento->getreferencia(); ?></p>
+                <p><?php echo $instrumento->getdistribuidor(); ?></p>
+            </center>
+            <br>
+            <center>
+                <label>tipo</label>
+                <p><?php echo $instrumento->gettipo(); ?></p>
+            </center>
+            <br>
+            <center>
+                <label>precio</label>
+                <p><?php echo $instrumento->getprecio(); ?></p>
+            </center>
+            <br>
+            <center>
+                <label>descuento</label>
+                <p><?php echo $instrumento->getdescuento(); ?></p>
+            </center>
+            <br>
+            <center>
+                <label>stockinicial</label>
+                <p><?php echo $instrumento->getstockinicial(); ?></p>
+            </center>
+            <hr class="colorgraph">
 
-<label>Distribuidor</label>
-
-<p><?php echo $instrumento->getdistribuidor(); ?></p>
-
-<label>tipo</label>
-<p><?php echo $instrumento->gettipo(); ?></p>
-
-<label>precio</label>
-<p><?php echo $instrumento->getprecio(); ?></p>
-
-<label>descuento</label>
-<p><?php echo $instrumento->getdescuento(); ?></p>
-
-<label>stockinicial</label>
-<p><?php echo $instrumento->getstockinicial(); ?></p>
-
-<p><a href="../index.php"> Aceptar</a></p>
-<br><br><br>
+            <p>
+            <div class="col-xs-12 col-md-6"><a href="../index.php" class="btn btn-primary btn-block btn-lg"> Aceptar</a>
+            </p>
+        </div>

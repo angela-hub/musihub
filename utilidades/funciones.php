cf410fffa50e3278eddfc97d5c8b21398ef3b18a
Gregorio <glopezmilla@gmail.com>
2020-02-06 00:20:42 +0100

    Funciones.php comentado

--- utilidades/funciones.php
+++ utilidades/funciones.php
@@ -18,7 +18,7 @@ function encode($str){
 function decode($str){
     return base64_decode(urldecode($str));
 }
-
+//Creamos la funcion para coger una cadena y ver si empieza por lo que se le pasa en el primer parametro
 function startsWith ($string, $startString){ 
     $len = strlen($startString); 
     return (substr($string, 0, $len) === $startString); 

d50b2d33cccb22c843ee7d170d8c3156e749cd6d
angela <angelahidort@gmail.com>
2020-02-17 22:58:41 +0100

    procesando fincarrito

--- carrito/fincarrito.php
+++ carrito/fincarrito.php
@@ -53,17 +53,121 @@ if (isset($_POST['procesar_compra'])) {
 }
 */
 ?>
-<a href='pago.php' class='btn btn-success' style="font-weight: bold">Continuar</a>
-<br><br>
-<div class="row cart-body">
-    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post" class="form-horizontal">
-        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-md-push-6 col-sm-push-6">
-            <div class="panel panel-default">
-                <div class="panel-heading">Pedido
-                    <div class="pull-right">
-                        <small><a href='resumen.php'>Editar</a></small>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Proc. Pedido</title>
+    <link rel="icon" type="image/png" href="logo.png">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <style type="text/css">
+        .colorgraph {
+            height: 5px;
+            border-top: 0;
+            background: #c4e17f;
+            border-radius: 5px;
+            background-image: -webkit-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+            background-image: -moz-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+            background-image: -o-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+            background-image: linear-gradient(to right, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+        }
+    </style>
+</head>
+<?php
+$nombre = $email = $telefono = $direccion = "";
+$nombreErr = $emailErr = $telefonoErr = $direccionErr ="";
+
+// Procesamos el nombre
+$nombre = filtrado(($_POST["nombre"]));
+if(empty($nombre)){
+    $nombreErr = "Por favor introduzca un nombre válido con solo carávteres alfabéticos.";
+} elseif(!preg_match("/([^\s][A-zÀ-ž\s]+$)/", $nombre)) {
+    $nombreErr = "Por favor introduzca un nombre válido con solo carávteres alfabéticos.";
+} else{
+    $nombre= $nombre;
+}
+
+// Procesamos el email
+$email = filtrado($_POST["email"]);
+if(empty($email)){
+    $emailErr = "Por favor introduzca email válido.";
+} else{
+    $email= $email;
+}
+
+// Procesamos el teléfono
+$telefono= filtrado($_POST["telefono"]);
+if(empty($telefono)){
+    $telefonoErr="Por favor Introduzca un telefono válido";
+}elseif(!filter_var($telefono, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/[0-9]{9}/")))){
+    $telefonoErr="Por favor intruduzca un teléfono válida con el formato NNNNNNNNN";
+}else{
+    $telefono=$telefono;
+}
+
+// Procesamos la direccion
+$direccion = filtrado(($_POST["direccion"]));
+if(empty($direccion)){
+    $direccionErr = "Por favor introduzca un nombre válido con solo carávteres alfabéticos.";
+} elseif(!preg_match("/([^\s][A-zÀ-ž\s]+$)/", $direccion)) {
+    $direccionErr = "Por favor introduzca un nombre válido con solo carávteres alfabéticos.";
+} else{
+    $direccion= $direccion;
+}
+?>
+
+<div class="container">
+<div class="row">
+        <form role="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+            <center><h2>Datos <small>Rellenalos para el envío</small></h2></center>
+            <hr class="colorgraph">
+
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-6" <?php echo (!empty($nombreErr)) ? 'error: ' : ''; ?>">
+                    <div class="form-group">
+                        <input type="text" required name="nombre" class="form-control input-lg" placeholder="Nombre" tabindex="1" value="<?php echo $nombre; ?>">
+                        <span class="help-block"><?php echo $nombreErr; ?></span>
                     </div>
                 </div>
+
+                <div class="col-xs-12 col-sm-6 col-md-6" <?php echo (!empty($emailErr)) ? 'error: ' : ''; ?>">
+                <input type="email" required name="email" class="form-control input-lg" placeholder="E-mail" tabindex="3" value="<?php echo $email; ?>">
+                <span class="help-block"><?php echo $emailErr; ?></span>
+            </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-6" <?php echo (!empty($telefonoErr)) ? 'error: ' : ''; ?>">
+                    <input type="tel" required name="telefono" class="form-control input-lg" placeholder="Telefono" tabindex="5" value="<?php echo $telefono; ?>" pattern="[0-9]{9}" title="Debes poner 9 números">
+                    <span class="help-block"><?php echo $telefonoErr; ?></span>
+                </div>
+
+                <div class="col-xs-12 col-sm-6 col-md-6" <?php echo (!empty($direccionErr)) ? 'error: ' : ''; ?>">
+                    <div class="form-group">
+                        <input type="text-area" required name="direccion" class="form-control input-lg" placeholder="Direccion" tabindex="1" value="<?php echo $direccion; ?>">
+                        <span class="help-block"><?php echo $direccionErr; ?></span>
+                    </div>
+                </div>
+            </div>
+            <hr class="colorgraph">
+            <div class="row">
+                <div class="col-xs-12 col-md-6"><a href="resumen.php" class="btn btn-warning btn-block btn-lg">Atrás</a></div>
+                <div class="col-xs-12 col-md-6"><a href="pago.php" class="btn btn-success btn-block btn-lg">Pago</a></div>
+            </div>
+        </form>
+    </div>
+</div>
+</div>
+
+<br><br>
+
+    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post" class="form-horizontal">
+            <div class="panel panel-default">
+                <div class="panel-heading">Resumen de su pedido</div>
                 <div class="panel-body">
                     <?php
 

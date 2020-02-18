bfb5be84445ebf88cce5d7c99d9402874668be20
angela <angelahidort@gmail.com>
2020-02-18 10:29:22 +0100

    Restricciones para fincarrito

--- carrito/fincarrito.php
+++ carrito/fincarrito.php
@@ -65,90 +65,74 @@ if (isset($_POST['procesar_compra'])) {
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <style type="text/css">
-        .colorgraph {
-            height: 5px;
-            border-top: 0;
-            background: #c4e17f;
-            border-radius: 5px;
-            background-image: -webkit-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
-            background-image: -moz-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
-            background-image: -o-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
-            background-image: linear-gradient(to right, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
-        }
-    </style>
 </head>
 <?php
 $nombre = $email = $telefono = $direccion = "";
 $nombreErr = $emailErr = $telefonoErr = $direccionErr ="";
 
-// Procesamos el nombre
-$nombre = filtrado(($_POST["nombre"]));
-if(empty($nombre)){
-    $nombreErr = "Por favor introduzca un nombre válido con solo carávteres alfabéticos.";
-} elseif(!preg_match("/([^\s][A-zÀ-ž\s]+$)/", $nombre)) {
-    $nombreErr = "Por favor introduzca un nombre válido con solo carávteres alfabéticos.";
-} else{
-    $nombre= $nombre;
-}
+if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["pago"]){
 
-// Procesamos el email
-$email = filtrado($_POST["email"]);
-if(empty($email)){
-    $emailErr = "Por favor introduzca email válido.";
-} else{
-    $email= $email;
-}
+    // Procesamos el nombre
+    $nombre=$_POST["nombre"];
+    if(isset($_POST["nombre"]) && !preg_match("/^([A-Za-zÑñ]+[áéíóú]?[A-Za-z]*){3,18}\s+([A-Za-zÑñ]+[áéíóú]?[A-Za-z]*){3,36}$/iu", $nombre)) { 
+        $nombreErr = "Introduzca un nombre y apellidos";
+      }
 
-// Procesamos el teléfono
-$telefono= filtrado($_POST["telefono"]);
-if(empty($telefono)){
-    $telefonoErr="Por favor Introduzca un telefono válido";
-}elseif(!filter_var($telefono, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/[0-9]{9}/")))){
-    $telefonoErr="Por favor intruduzca un teléfono válida con el formato NNNNNNNNN";
-}else{
-    $telefono=$telefono;
-}
+    // Procesamos el email
+    $email = filtrado($_POST["email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Por favor introduzca email válido.";
+    }
 
-// Procesamos la direccion
-$direccion = filtrado(($_POST["direccion"]));
-if(empty($direccion)){
-    $direccionErr = "Por favor introduzca un nombre válido con solo carávteres alfabéticos.";
-} elseif(!preg_match("/([^\s][A-zÀ-ž\s]+$)/", $direccion)) {
-    $direccionErr = "Por favor introduzca un nombre válido con solo carávteres alfabéticos.";
-} else{
-    $direccion= $direccion;
+    // Procesamos el teléfono
+    $telefono= filtrado($_POST["telefono"]);
+    if(empty($telefono)){
+        $telefonoErr="Por favor Introduzca un numero de telefono";
+    }elseif(!filter_var($telefono, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/[0-9]{9}/")))){
+        $telefonoErr="Por favor intruduzca un teléfono válido";
+    }
+
+    // Procesamos la direccion
+    $direccion = filtrado(($_POST["direccion"]));
+    if(empty($direccion)){
+        $direccionErr = "Por favor introduzca una direccion";
+    } elseif(!preg_match("/[a-zA-Z1-9À-ÖØ-öø-ÿ]+\.?(( |\-)[a-zA-Z1-9À-ÖØ-öø-ÿ]+\.?)* (((#|[nN][oO]\.?) ?)?\d{1,4}(( ?[a-zA-Z0-9\-]+)+)?)/", $direccion)) {
+        $direccionErr = "Por favor introduzca una direccion válida";
+    }
 }
 ?>
 
 <div class="container">
-<div class="row">
+    <div class="row">
         <form role="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
             <center><h2>Datos <small>Rellenalos para el envío</small></h2></center>
             <hr class="colorgraph">
 
             <div class="row">
-                <div class="col-xs-12 col-sm-6 col-md-6" <?php echo (!empty($nombreErr)) ? 'error: ' : ''; ?>">
+                <div class="col-xs-12 col-sm-6 col-md-6" <?php echo (!empty($nombreErr)) ? 'error: ' : ''; ?>>
                     <div class="form-group">
-                        <input type="text" required name="nombre" class="form-control input-lg" placeholder="Nombre" tabindex="1" value="<?php echo $nombre; ?>">
+                        <input type="text" required name="nombre" pattern="([^\s][A-zÀ-ž\s]+)" class="form-control input-lg" placeholder="Nombre" tabindex="1">
                         <span class="help-block"><?php echo $nombreErr; ?></span>
                     </div>
                 </div>
 
-                <div class="col-xs-12 col-sm-6 col-md-6" <?php echo (!empty($emailErr)) ? 'error: ' : ''; ?>">
-                <input type="email" required name="email" class="form-control input-lg" placeholder="E-mail" tabindex="3" value="<?php echo $email; ?>">
+                <div class="col-xs-12 col-sm-6 col-md-6" <?php echo (!empty($emailErr)) ? 'error: ' : ''; ?>>
+                <input type="email" required name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" class="form-control input-lg" 
+                placeholder="E-mail" tabindex="3">
                 <span class="help-block"><?php echo $emailErr; ?></span>
             </div>
             </div>
             <div class="row">
-                <div class="col-xs-12 col-sm-6 col-md-6" <?php echo (!empty($telefonoErr)) ? 'error: ' : ''; ?>">
-                    <input type="tel" required name="telefono" class="form-control input-lg" placeholder="Telefono" tabindex="5" value="<?php echo $telefono; ?>" pattern="[0-9]{9}" title="Debes poner 9 números">
+                <div class="col-xs-12 col-sm-6 col-md-6" <?php echo (!empty($telefonoErr)) ? 'error: ' : ''; ?>>
+                    <input type="tel" required name="telefono" class="form-control input-lg" placeholder="Telefono" tabindex="5" pattern="[0-9]{9}" 
+                    title="Debes poner 9 números">
                     <span class="help-block"><?php echo $telefonoErr; ?></span>
                 </div>
 
-                <div class="col-xs-12 col-sm-6 col-md-6" <?php echo (!empty($direccionErr)) ? 'error: ' : ''; ?>">
+                <div class="col-xs-12 col-sm-6 col-md-6" <?php echo (!empty($direccionErr)) ? 'error: ' : ''; ?>>
                     <div class="form-group">
-                        <input type="text-area" required name="direccion" class="form-control input-lg" placeholder="Direccion" tabindex="1" value="<?php echo $direccion; ?>">
+                        <input type="text-area" required name="direccion" pattern="[a-zA-Z1-9À-ÖØ-öø-ÿ]+\.?(( |\-)[a-zA-Z1-9À-ÖØ-öø-ÿ]+\.?)* (((#|[nN][oO]\.?) ?)?\d{1,4}(( ?[a-zA-Z0-9\-]+)+)?)" 
+                        class="form-control input-lg" placeholder="Direccion" tabindex="1">
                         <span class="help-block"><?php echo $direccionErr; ?></span>
                     </div>
                 </div>
@@ -156,55 +140,54 @@ if(empty($direccion)){
             <hr class="colorgraph">
             <div class="row">
                 <div class="col-xs-12 col-md-6"><a href="resumen.php" class="btn btn-warning btn-block btn-lg">Atrás</a></div>
-                <div class="col-xs-12 col-md-6"><a href="pago.php" class="btn btn-success btn-block btn-lg">Pago</a></div>
+                <div class="col-xs-12 col-md-6"><a href="pago.php" type="submit" name="pago" class="btn btn-success btn-block btn-lg">Pago</a></div>
             </div>
         </form>
     </div>
 </div>
-</div>
 
 <br><br>
 
-    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post" class="form-horizontal">
-            <div class="panel panel-default">
-                <div class="panel-heading">Resumen de su pedido</div>
-                <div class="panel-body">
-                    <?php
+<div class="panel panel-default">
+    <div class="panel-heading">Resumen de su pedido</div>
+    <div class="panel-body">
+        <?php
 
-                    if(isset($_SESSION['carrito']['final']) && !empty($_SESSION['carrito']['final'])){
-                        $arreglo=$_SESSION['carrito']['final'];
-                        echo "<table><th></th><th>Instrumento</th><th>Distribuidor</th><th>Precio</th><th>Cantidad</th>";
-                        foreach ($arreglo as $key => $fila){
-                            if($fila['cantidad']>=1){
-                            $foto = $fila['foto'];
-                            echo "<tr><td><img src='../imagenes/fotos/" . $foto . "' width='70px' height='70'></td>";
-                            echo "<td>" . $fila['nomProducto'] . "</td>";
-                            echo "<td>" . $fila['marca'] . "</td>";
-                            echo "<td>" . $fila['precio'] . "</td>";
-                            echo "<td>" . $fila['cantidad'] . "</td>";
-                            }
-                        }
-                    ?>
-                    <?php
-                        }
-                        // Subtotales y totales
-                        $final=array_sum($_SESSION['total']);
-                        $_SESSION['precio']=$final;
-                        echo "<tr>";
-                        //Calculo del precio sin iva
-                        $sub=$final/1.21;
-                        //calculo del precio con iva
-                        $iva=$final-($final/1.21);
+        if(isset($_SESSION['carrito']['final']) && !empty($_SESSION['carrito']['final'])){
+            $arreglo=$_SESSION['carrito']['final'];
+            echo "<table><th></th><th>Instrumento</th><th>Distribuidor</th><th>Precio</th><th>Cantidad</th>";
+            foreach ($arreglo as $key => $fila){
+                if($fila['cantidad']>=1){
+                $foto = $fila['foto'];
+                echo "<tr><td><img src='../imagenes/fotos/" . $foto . "' width='70px' height='70'></td>";
+                echo "<td>" . $fila['nomProducto'] . "</td>";
+                echo "<td>" . $fila['marca'] . "</td>";
+                echo "<td>" . $fila['precio'] . "</td>";
+                echo "<td>" . $fila['cantidad'] . "</td>";
+                }
+            }
+        ?>
+        <?php
+            }
+            // Subtotales y totales
+            $final=array_sum($_SESSION['total']);
+            $_SESSION['precio']=$final;
+            echo "<tr>";
+            //Calculo del precio sin iva
+            $sub=$final/1.21;
+            //calculo del precio con iva
+            $iva=$final-($final/1.21);
 
-                        //Mostramos la tabla con los precios calculados y redondeados a dos decimales
-                        echo "<td>". "Subtotal" ."  ". round($sub,2) . " ". "€". "</td>";
-                        echo "<tr>";
-                        echo "<td>". "IVA" ."  ". round($iva,2). " ". "€". "</td>";
-                        echo "<tr>";
-                        echo "<td>". "Total" ."  ". $final. " ". "€". "</td>";
-                        echo "</tr>";
-                    ?>
-        
-            
+            //Mostramos la tabla con los precios calculados y redondeados a dos decimales
+            echo "<td>". "Subtotal" ."  ". round($sub,2) . " ". "€". "</td>";
+            echo "<tr>";
+            echo "<td>". "IVA" ."  ". round($iva,2). " ". "€". "</td>";
+            echo "<tr>";
+            echo "<td>". "Total" ."  ". $final. " ". "€". "</td>";
+            echo "</tr>";
+        ?>
+
+    </div>
+</div>
 
 

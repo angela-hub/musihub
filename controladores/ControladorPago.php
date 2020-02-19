1e5d2b38ed61513ea9f0626e0875a92444411939
Gregorio <glopezmilla@gmail.com>
2020-02-19 19:07:34 +0100

    ControladorPago modificado

--- controladores/ControladorPago.php
+++ controladores/ControladorPago.php
@@ -28,24 +28,5 @@ public function almacenarpago($titular,$tarjeta_completa,$fecha,$cv){
     }
 //-----------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------Funcion para almacenar carrito-----------------------------------------------
-public function almacenarcarrrito($nombre,$distribuidor,$precio,$cantidad){
-    $bd = ControladorBD::getControlador();
-    $bd->abrirBD();
-    $consulta= "INSERT INTO carrito (nombre, distribuidor, precio, cantidad) VALUES (:nombre, :distribuidor, :precio, :cantidad)";
-    $parametros= array(':nombre'=>$nombre, ':distribuidor'=>$distribuidor, ':precio'=>$precio, ':cantidad'=>$cantidad);
-    $estado = $bd->actualizarBD($consulta,$parametros);
-    $bd->cerrarBD();
-    return $estado;
-}
-//-----------------------------------------------------------------------------------------------------------------------------
-public function almacenarventa($nombre,$email,$telefono,$domicilio,$total,$fecha){
-    $bd = ControladorBD::getControlador();
-    $bd->abrirBD();
-    $consulta= "INSERT INTO ventas (nombre, distribuidor, precio, cantidad) VALUES (:nombre, :distribuidor, :precio, :cantidad)";
-    $parametros= array(':nombre'=>$nombre, ':distribuidor'=>$distribuidor, ':precio'=>$precio, ':cantidad'=>$cantidad);
-    $estado = $bd->actualizarBD($consulta,$parametros);
-    $bd->cerrarBD();
-    return $estado;
-}
 }
 ?>
